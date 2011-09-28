fa8f3c1f8c820cb446a83e46900c829ddf2691de
MatToufoutu <mattoufootu@gmail.com>
2011-09-28 17:35:23 +0200

    remove local_settings file

--- feedme/local_settings.py
+++
@@ -1,3 +0,0 @@
-SEC_KEY = '3[!Ui&sLCeJaI+8$ / oVIr}@>pTT;z2k=VIm).~5/nf\'fXlD^iCu)_x1nz):4kCf\n"(8/N{q!fE\x0b\'QtU\nrnK]`"Gxb\rmk{Te=>Kf16JMKcbH\\B\rG$TJ(\x0c@<$cE*#}ef'
-MAIL_USERS = ['mattoufootu@gmail.com']
-
